2bbc1bf4ef3b62e12ddb7b821e7dc972f4d2c0bb
Neil <achacoso.neil@gmail.com>
2018-08-01 15:38:09 +0800

    index

--- index.php
+++ index.php
@@ -7,10 +7,17 @@
 	<title></title>
 </head>
 <body>
-	Hello World!!
+	<div class="container">
+		<div class="alert alert-primary">
+			This is a primary alert—check it out!
+		</div>
+	</div>
 
 
 
+
+
+	<!---scripts--->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="boostrap/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
