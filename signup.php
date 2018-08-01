701e31c05c225057f5f7b209aa39f687bdfa109d
Neil <achacoso.neil@gmail.com>
2018-08-01 17:30:04 +0800

    added navbar to all files

---
+++ signup.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css" />
+	<title>ddd</title>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+		<div class="container">
+	  		<a class="navbar-brand" href="#">
+	  			<img src="images/logo.png" id="logo" width="150" alt="">
+	  		</a>
+	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    		<span class="navbar-toggler-icon"></span>
+	  		</button>
+
+	  		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    		<ul class="navbar-nav ml-auto">
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="about.php">About</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="media.php">Tutorials</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="portfolio.php">Portfolio</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="jobs.php">Jobs</a>
+	      			</li>
+	      			<li class="nav-item">
+	        			<a class="nav-link" href="#">Contact</a>
+	      			</li>
+	      			<li class="nav-item dropdown">
+        				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          					Login
+        				</a>
+        				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				        	<a class="dropdown-item" href="#">Sign Up</a>
+        				</div>
+      				</li>
+	    		</ul>
+	  		</div>
+	  	</div>
+	</nav>
+
+
+
+
+
+	<!---scripts--->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="bootstrap/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
