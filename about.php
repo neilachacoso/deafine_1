f5b195bd7eedfd48559de90a97ec42545e08433b
Neil <achacoso.neil@gmail.com>
2018-08-01 17:54:12 +0800

    about.php

--- about.php
+++ about.php
@@ -56,10 +56,20 @@
   		</div>	
 	</div>
 
+	<div class="container text-center">
+		<div class="embed-responsive embed-responsive-16by9">
+  			<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+		</div>						
+		<h1>ABOUT</h1>
+		<p>DEAFine promotes employabitlity of deaf artists. Here, the deaf will be able to easily find employers who are willing to hire deaf applicants. We promote the importance of seeing the skill of the applicants rather than focusing of their disability. DEAFine only serves as a bridge of connection between deaf employees and the employers. All forms of communication of the users will not be managed by the website. Any problems that you encounter while hiring or being hired that was not done in the site will not be under our responsibility.</p>
+						
+		<h1>REMINDERS</h1>
+		<p>The Applicants must be deaf or hard of hearing. Your profile and Information on this site must be real. Any job you apply for from this site is only recommended by the site, we do not hold any responsibility for them nor the decisions you make with them. We are not held responsible if you encounter problems beyond this site. The applicant on this site are part of the deaf community, please have the patience of understanding them. Insulting, degrading or use of vulgar words or discrimination is forbidden. The jobs and Information you post must be real. We only serve as a bridge to employers and the deaf applicants, any problems you encounter beyond this site, we are not held responsible for.</p>
+	</div>	
 
 	<!---scripts--->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="bootstrap/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
